018b0712d72e2c589e92bd16f10f836bbcbf1daf
Sarai75 <moaj080602mmcrlsa3@cbtis217.edu.mx>
2024-11-29 12:38:28 -0600

    Grafica años

--- amanecer dorado/amanecer dorado/Form2.Designer.cs
+++ amanecer dorado/amanecer dorado/Form2.Designer.cs
@@ -36,15 +36,18 @@ namespace amanecer_dorado
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
 			this.button1 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.button2 = new System.Windows.Forms.Button();
 			this.label2 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(539, 400);
+			this.button1.Location = new System.Drawing.Point(772, 430);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 8;
@@ -54,18 +57,18 @@ namespace amanecer_dorado
 			// 
 			// label1
 			// 
-			this.label1.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Font = new System.Drawing.Font("MV Boli", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.ForeColor = System.Drawing.Color.DarkKhaki;
-			this.label1.Location = new System.Drawing.Point(185, 41);
+			this.label1.Location = new System.Drawing.Point(265, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(218, 38);
+			this.label1.Size = new System.Drawing.Size(297, 51);
 			this.label1.TabIndex = 9;
 			this.label1.Text = "ESTADISTICAS";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(12, 400);
+			this.button2.Location = new System.Drawing.Point(12, 430);
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 12;
@@ -75,29 +78,44 @@ namespace amanecer_dorado
 			// 
 			// label2
 			// 
-			this.label2.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label2.ForeColor = System.Drawing.Color.DarkKhaki;
-			this.label2.Location = new System.Drawing.Point(203, 91);
+			this.label2.Location = new System.Drawing.Point(12, 73);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(218, 38);
+			this.label2.Size = new System.Drawing.Size(835, 90);
 			this.label2.TabIndex = 13;
-			this.label2.Text = "Aquí veremos estasdisticas ";
+			this.label2.Text = "Aquí veremos una estasdistica de los accidentes que han ocurrido con el paso de l" +
+			"os años desde el 2021, teniendo en cuenta que es el año donde empezo a haber más" +
+			" accidentes.";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(128, 179);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(599, 259);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
 			// 
 			// Form2
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.ClientSize = new System.Drawing.Size(626, 445);
+			this.ClientSize = new System.Drawing.Size(859, 465);
+			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
 			this.Name = "Form2";
 			this.Text = "Form2";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button button1;
